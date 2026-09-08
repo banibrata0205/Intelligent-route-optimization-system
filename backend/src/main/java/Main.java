d61b04f96583c477c2e9571bbff6c6a322417f32
Banibrata <janabanibrata9@gmail.com>
2026-09-09 00:07:48 +0530

    Update application startup message

--- backend/src/main/java/Main.java
+++ backend/src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Intelligent Route Optimization System");
+        System.out.println("Intelligent Route Optimization System Started");
 
     }
 }
